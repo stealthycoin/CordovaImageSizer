2347b47be41eb6f1521b84f8996f753d26d25993
stealthycoin <john.w.carlyle@gmail.com>
2015-01-02 00:41:29 -0800

    Initial content

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-CordovaImageSizer
+Cordova Image Sizer
 =================
 
 Resizes an image to a bunch of different sizes and export them to many locations. Currently using it to generate and place android/iOS icons and splash screens.
+
+Requirements
+-----------------
+ImageMagick
+python
---
+++ images.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from json import loads
+from subprocess import call
+sets = []
+with open('images/images.json') as f:
+    sets = loads(f.read())
+
+for s in sets:
+    print "Processing " + s["name"]
+    for target in s["targets"]:
+        data = s["targets"][target]
+        cmd = []
+        if data["type"] == "scale":
+            cmd = ["convert", s["source"], "-resize", data["size"], s["path"] + target]
+        elif data["type"] == "scale-crop":
+            w,h = int(data["size"].split("x")[0]), int(data["size"].split("x")[1])
+            resize = "x" + str(h)
+            if w > h:
+                resize = str(w) + "x"
+            cmd = ["convert", s["source"], "-resize", resize, "-gravity", "Center", "-crop", data["size"]+"+0+0", "+repage", s["path"] + target]
+        elif data["type"] == "crop":
+            cmd = ["convert", s["source"], "-gravity", "Center", "-crop", data["size"]+"+0+0", "+repage", s["path"] + target]
+
+        call(cmd)
